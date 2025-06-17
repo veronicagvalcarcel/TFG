c31cb82b3b88d943a3afb82ece25c65174a8c30e
veronicagvalcarcel <veronicagelabert8@gmail.com>
2025-06-17 14:18:04 +0200

    Fix: duplicated video 1.mp4

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -1,24 +1,13 @@
 <template>
   <header>
     <div class="header-container">
-      <h1>{{ title }}</h1>
-      <video :src="videoUrl" autoplay loop muted class="header-video">
-        <source :src="videoUrl" type="video/mp4">
-      </video>
+      <h1>Witch Tattoo</h1>
     </div>
   </header>
 </template>
 
 <script>
-export default {
-
-  data() {
-    return {
-      title: 'Witch Tattoo',
-      videoUrl: '/public/1.mp4'
-    }
-  }
-}
+export default {}
 </script>
 
 <style scoped>
@@ -26,20 +15,8 @@ export default {
   position: relative;
   width: 100%;
   height: 100vh;
-  background-color: #fff;
+  background-color: transparent;
 }
-
-.header-video {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  z-index: 1; /* Cambiado a 1 para que el video esté encima de otros elementos */
-  visibility: visible; /* Agregado para asegurarse de que el video esté visible */
-}
-
 h1 {
   position: absolute;
   top: 50%;
@@ -48,6 +25,6 @@ h1 {
   font-size: 48px;
   color: #fff;
   text-align: center;
-  z-index: 2; /* Agregado para asegurarse de que el título esté encima del video */
+  z-index: 2;
 }
 </style>
